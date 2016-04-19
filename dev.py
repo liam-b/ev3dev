f91ff930304658bd0d218df12b0a0c0871faf531
Liam <liam@anywhich.com>
2016-04-19 05:41:14 +0000

    alpha 0.1 build

--- dev.py
+++ dev.py
@@ -4,13 +4,24 @@ import time
 
 rightMotor = ev3.LargeMotor('outA')
 
+build = 'alpha 0.1'
+running = True
+localTime = time.asctime(time.localtime(time.time()))
+
+logFile = open('log.txt', 'w')
+def log(text, status):
+    logFile.write('[' + status + '] ' + text + '\n')
+    
+log('logging: ' + localTime,'OUT')
+log('running build: ' + build,'OUT')
+
 def runRight(speed):
     rightMotor.run_forever(duty_cycle_sp = speed)
     
 def stopRight():
     rightMotor.stop()
 
-runRight(100)
-time.sleep(3)
-stopRight()
+# runRight(100)
+# time.sleep(3)
+# stopRight()
 time.sleep(3)
