d5046d4c8ab64e2bcfc05c0a972fc2e6f099ab5e
Liam <liam@anywhich.com>
2016-04-21 10:06:14 +0000

    running test

--- test.py
+++ test.py
@@ -15,34 +15,18 @@ class Motor():
     def stop(self):
         self.motor.stop()
         
-class ColorSensor():
-    def __init__(self, port):
-        self.port = port
-        self.sensor = ev3.ColorSensor(port)
-        
 motorRight = Motor('outA')
-colorRight = ColorSensor('in1')
 
-build = 'alpha 0.3'
+# rightMotor = ev3.LargeMotor('outA')
+# colorRight = ev3.ColorSensor('in1')
+
 running = True
-localTime = time.strftime('%c')
-logFile = open('output.log', 'w')
 
-def log(text):
-    logFile.write(text + '\n')
-    
 def sleep(delay):
     time.sleep(delay / 1000)
 
-#####
-
-def start():
-    log(localTime + ' | ' + build + '\n')
-
 def loop():
     motorRight.run(50)
-
-start()
 
 while running:
     loop()
